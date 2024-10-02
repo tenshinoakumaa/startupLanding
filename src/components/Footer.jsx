c73bcbbd2964cc617e808d7c754c6c52e86100b5
moshi6kikiyama <moshi6kikiyama@mail.ru>
2024-10-02 09:52:05 +0500

    add modal

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -23,6 +23,18 @@ export default function Footer() {
     const closeModal = () => {
         setModalIsOpen(false);
     };
+
+    const [modalIsOpen2, setModalIsOpen2] = useState(false);
+
+    const openModal2 = () => {
+        setModalIsOpen2(true);
+    };
+
+    const closeModal2 = () => {
+        setModalIsOpen2(false);
+    };
+
+
     return <>
         <div style={{ backgroundImage: `url(${mainPageImage})`, backgroundSize: 'cover' }} className="lg:hidden bg-black py-28 px-4 sm:px-28" id="footer">
             <div className='flex justify-between mb-12 space-y-4 text-white w-full'>
@@ -95,23 +107,23 @@ export default function Footer() {
                 </div>
             </div>
             <div className='mx-auto mt-[40px] max-w-[470px] bg-white flex flex-col items-center px-[25px] py-[40px] space-y-[32px]'>
-                    <span className='text-[#025699]' style={{
-                        fontFamily: 'Jost',
-                        fontSize: '40px',
-                        fontWeight: '700',
-                        lineHeight: '57.8px',
-                        textAlign: 'left',
-                    }}>
-                        Оставьте заявку
-                    </span>
-                    <input type="text" placeholder='Имя' className='w-full rounded-[10px] shadow-2xl bg-[#5088B5] text-white text-[24px] placeholder-white p-[17px]' />
-                    <input type="text" placeholder='Почта' className='bg-[#5088B5] w-full rounded-[10px] shadow-2xl text-white text-[24px] placeholder-white p-[17px]' />
-                    <input type="text" placeholder='+7 (000)000 00 00' className='bg-[#5088B5] w-full rounded-[10px] shadow-2xl text-white text-[24px] placeholder-white p-[17px]' />
-                    <div className='w-full bg-[#025699] text-[35px] text-white px-[48px] text-center py-[17px]'>Отправить заявку</div>
-                    <div className='text-black'>
-                        Оставляя заявку Вы даете согласие на информационную рассылку ваших данных
-                    </div>
+                <span className='text-[#025699]' style={{
+                    fontFamily: 'Jost',
+                    fontSize: '40px',
+                    fontWeight: '700',
+                    lineHeight: '57.8px',
+                    textAlign: 'left',
+                }}>
+                    Оставьте заявку
+                </span>
+                <input type="text" placeholder='Имя' className='w-full rounded-[10px] shadow-2xl bg-[#5088B5] text-white text-[24px] placeholder-white p-[17px]' />
+                <input type="text" placeholder='Почта' className='bg-[#5088B5] w-full rounded-[10px] shadow-2xl text-white text-[24px] placeholder-white p-[17px]' />
+                <input type="text" placeholder='+7 (000)000 00 00' className='bg-[#5088B5] w-full rounded-[10px] shadow-2xl text-white text-[24px] placeholder-white p-[17px]' />
+                <div onClick={openModal2} className='w-full bg-[#025699] text-[35px] text-white px-[48px] text-center py-[17px]'>Отправить заявку</div>
+                <div className='text-black'>
+                    Оставляя заявку Вы даете согласие на информационную рассылку ваших данных
                 </div>
+            </div>
             <div style={{
                 fontSize: '25px',
                 fontWeight: '500',
@@ -135,7 +147,7 @@ export default function Footer() {
                     <input type="text" placeholder='Имя' className='w-full rounded-[10px] shadow-2xl bg-[#5088B5] text-white text-[24px] placeholder-white p-[17px]' />
                     <input type="text" placeholder='Почта' className='bg-[#5088B5] w-full rounded-[10px] shadow-2xl text-white text-[24px] placeholder-white p-[17px]' />
                     <input type="text" placeholder='+7 (000)000 00 00' className='bg-[#5088B5] w-full rounded-[10px] shadow-2xl text-white text-[24px] placeholder-white p-[17px]' />
-                    <div className='w-full bg-[#025699] text-[35px] text-white px-[48px] text-center py-[17px]'>Отправить заявку</div>
+                    <div onClick={openModal2} className='w-full bg-[#025699] text-[35px] text-white px-[48px] text-center py-[17px]'>Отправить заявку</div>
                     <div className='text-black'>
                         Оставляя заявку Вы даете согласие на информационную рассылку ваших данных
                     </div>
@@ -214,6 +226,49 @@ export default function Footer() {
                 <span onClick={openModal}>© 2024 . Политика конфиденциальности</span>
             </div>
         </div >
+
+        <Modal className='mx-auto flex items-center justify-center h-full' isOpen={modalIsOpen2} onRequestClose={closeModal2}>
+            <ModalContent className='max-w-[1071px]  bg-white flex flex-col items-center h-[800px] lg:h-[450px]' >
+                <div className='flex  lg:items-center lg:space-x-[40px] flex-col items-center space-y-[20px] lg:flex-row lg:justify-between w-full'>
+                    <div className='px-[35px] pt-[12px] pb-[64px] bg-[#025699] rounded-[15px] space-y-[89px]'>
+                        <div className='text-end text-white italic font-bold'>
+                            CARD
+                        </div>
+                        <div className='text-center text-white italic text-[35px] '>
+                            1234 56XX XXXX XXXX
+                        </div>
+                    </div>
+                    <div className='flex flex-col space-y-[37px]'>
+                        <input type='text' style={
+                            {
+                                border: '1px solid #00000029'
+                            }
+                        } placeholder='Номер карты' className='py-[17px] px-[20px] text-[#989595] text-[24px] max-w-[420px]' />
+                        <input
+                            style={
+                                {
+                                    border: '1px solid #00000029'
+                                }
+                            }
+                            type='text' placeholder='ММ/ГГ' className='py-[17px] px-[20px] text-[#989595] text-[24px] max-w-[420px]' />
+                        <input
+                            style={
+                                {
+                                    border: '1px solid #00000029'
+                                }
+                            }
+                            type='text' placeholder='CVV' className='py-[17px] px-[20px] text-[#989595] text-[24px] max-w-[420px]' />
+
+                        <div className='bg-[#025699] py-[17px] w-[full] text-center text-white text-[24px]'>
+                            Оформить заказ
+                        </div>
+                    </div>
+                </div>
+
+            </ModalContent>
+        </Modal>
+
+
 
         <Modal style={{
 
